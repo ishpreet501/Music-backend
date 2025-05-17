981185fca058d53623d8ca0957a672acfdcf21b4
ishpreet501 <sishpreet135@gmail.com>
2025-05-17 15:50:09 +0530

    Url Changed

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const discographyRouter = require("./Routes/discography");
 const port = 5000;
 const server = express()
 
-server.use(cors())
+server.use(cors({
+    origin:["https://rad-douhua-2ff8b3.netlify.app/","http://localhost:5173"],
+    credentials:true
+}
+))
 server.use(express.json());
 dbconnection()
 server.use('/api', adminrouter);
